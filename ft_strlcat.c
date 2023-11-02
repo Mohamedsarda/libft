0e9218cddd8d8c85d22a0d513eee086b422e60d1
Mohamed Sarda <msarda@e1r11p1.1337.ma>
2023-11-02 20:04:24 +0100

    strlcat is wrong

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -11,23 +11,23 @@
 /* ************************************************************************** */
 
 #include "libft.h"
+#include <string.h>
 
 size_t	ft_strlcat(char *restrict dst, const char *restrict src, size_t dstsize)
 {
-	size_t	i;
+  size_t i = 0;
+  size_t d_len = 0;
+  size_t s_len = 0;
 
-	i = 0;
-	if (!dst)
-		return (ft_strlen(src));
-	if (dstsize > 0)
-	{
-		while (src[i] && (i < dstsize))
-		{
-			dst[i] = src[i];
-			i++;
-		}
-		if (i < dstsize)
-			dst[i] = '\0';
-	}
-	return (ft_strlen(dst));
+
+  return (ft_strlen(src) + ft_strlen(dst));
+}
+
+int main()
+{
+	char d[10] = "012345";
+	char s[] = "med sarda";
+	// printf("%zu \n", ft_strlcat(d, s, 10));
+	printf("%zu \n", strlcat(d, s, 10));
+	printf("%s \n", d);
 }
