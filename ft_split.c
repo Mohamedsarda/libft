b2f7cebcfa510e1db3492c1d1fe3d0df922173a4
Mohamed Sarda <msarda@e1r11p4.1337.ma>
2023-11-02 10:26:46 +0100

    m split

--- ft_split.c
+++ ft_split.c
@@ -1,66 +1,83 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_split.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: msarda <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/02 10:14:44 by msarda            #+#    #+#             */
+/*   Updated: 2023/11/02 10:23:38 by msarda           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 #include "libft.h"
 
-static int wordsCounter(char const *str, char c)
+static int	ft_words_counter(char const *str, char c)
 {
-    int i = 0;
-    int words = 0;
-    int is_word = 0;
+	int	i;
+	int	words;
+	int	is_word;
 
-    while (str[i])
-    {
-        if(str[i] == c)
-            is_word = 0;
-        else if(!is_word)
-        {
-            words++;
-            is_word = 1;
-        }
-        i++;
-    }
-    return (words);
+	i = 0;
+	words = 0;
+	is_word = 0;
+	while (str[i])
+	{
+		if (str[i] == c)
+			is_word = 0;
+		else if (!is_word)
+		{
+			words++;
+			is_word = 1;
+		}
+		i++;
+	}
+	return (words);
 }
 
-static int wordLen(char const *str, char c)
+static int	ft_word_len(char const *str, char c)
 {
-    int i = 0;
+	int	i;
 
-    while(str[i] && str[i] != c)
-        i++;
-    return (i);
+	i = 0;
+	while (str[i] && str[i] != c)
+		i++;
+	return (i);
 }
 
-static char **ft_putword(char const *str, char c, int words, char **dst)
+static char	**ft_putword(char const *str, char c, int words, char **dst)
 {
-    int i = 0;
-    int len = 0;
-    int j;
+	int	i;
+	int	len;
+	int	j;
 
-    while(i < words)
-    {
-        while(*str == c)
-            str++;
-        len = wordLen(str, c);
-        dst[i] = (char *)malloc(len * sizeof(char) + 1);
-        if (!dst)
-            return (NULL);
-        j = 0;
-        while(j < len)
-            dst[i][j++] = *str++;
-        dst[i][j] = '\0';
-        i++;
-    }
-    return (dst);
+	i = 0;
+	len = 0;
+	while (i < words)
+	{
+		while (*str == c)
+			str++;
+		len = ft_word_len(str, c);
+		dst[i] = (char *)malloc(len * sizeof(char) + 1);
+		if (!dst[i])
+			return (NULL);
+		j = 0;
+		while (j < len)
+			dst[i][j++] = *str++;
+		dst[i][j] = '\0';
+		i++;
+	}
+	return (dst);
 }
 
-char    **ft_split(char const *s, char c)
+char	**ft_split(char const *s, char c)
 {
-    char **dst;
-    int words;
+	char	**dst;
+	int		words;
 
-    words = wordsCounter(s, c);
-    dst = (char **)malloc(words * sizeof(char *) + 1);
-    if(!dst)
-        return (NULL);
-    dst = ft_putword(s, c , words, dst);
-    return (dst);
+	words = ft_words_counter(s, c);
+	dst = (char **)malloc(words * sizeof(char *) + 1);
+	if (!dst)
+		return (NULL);
+	dst = ft_putword(s, c, words, dst);
+	return (dst);
 }
--- test.c
+++ test.c
@@ -2,9 +2,11 @@
 
 int main()
 {
-    char **str;
-    str = ft_split("kjfjksjd,difuhk,fikj.dosahdoiadho", '.');
-    printf("%s \n", str[0]);
-    printf("%s \n", str[1]);
-    printf("%s \n", str[2]);
+	char **str;
+	str = ft_split("qwe.reqwr.med.asdasd", '.');
+	printf("%s \n", str[0]);
+	printf("%s \n", str[1]);
+	printf("%s \n", str[2]);
+	printf("%s \n", str[3]);
+	printf("%s \n", str[4]);
 }
