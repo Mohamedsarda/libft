b59e819428024f11af6730b5da8679db45b9b7b8
Mohamed Sarda <msarda@e1r10p4.1337.ma>
2023-11-02 15:25:28 +0100

    strmapi.c

---
+++ ft_strmapi.c
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strmapi.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: msarda <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/02 15:14:32 by msarda            #+#    #+#             */
+/*   Updated: 2023/11/02 15:14:33 by msarda           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strmapi(char const *s, char (*f)(unsigned int, char))
+{
+	int		s_len;
+	int		i;
+	char	*dst;
+
+	if(!s)
+		return (NULL);
+	s_len = ft_strlen(s);
+	dst = (char *)malloc(s_len * sizeof(char) + 1);
+	if (!dst)
+		return (NULL);
+	i = 0;
+	while (i < s_len)
+	{
+		dst[i] = f(i , s[i]);
+		i++;
+	}
+	dst[i] = '\0';
+	return (dst);
+}
--- libft.h
+++ libft.h
@@ -37,9 +37,9 @@ void	*ft_memcpy(void *restrict dst, const void *restrict src, size_t n);
 void	*ft_memmove(void *dst, const void *src, size_t len);
 void	*ft_memchr(const void *s, int c, size_t n);
 int		ft_atoi(const char *str);
-size_t	ft_strlcpy(char *restrict dst, const char *restrict src, 
+size_t	ft_strlcpy(char *restrict dst, const char *restrict src,
 			size_t dstsize);
-size_t	ft_strlcat(char *restrict dst, const char *restrict src, 
+size_t	ft_strlcat(char *restrict dst, const char *restrict src,
 			size_t dstsize);
 char	*ft_strnstr(const char *haystack, const char *needle, size_t len);
 int		ft_strncmp(const char *s1, const char *s2, size_t n);
@@ -51,9 +51,10 @@ char	*ft_strjoin(char const *s1, char const *s2);
 char	*ft_strtrim(char const *s1, char const *set);
 char	**ft_split(char const *s, char c);
 char	*ft_itoa(int n);
-//
+char	*ft_strmapi(char const *s, char (*f)(unsigned int, char));
 void    ft_putchar_fd(char c, int fd);
 void	ft_putstr_fd(char *s, int fd);
 void	ft_putendl_fd(char *s, int fd);
 void	ft_putnbr_fd(int n, int fd);
+
 #endif
