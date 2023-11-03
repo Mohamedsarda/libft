8555793cf628405db4908c6171c027307fc37475
Mohamed Sarda <msarda@e1r10p2.1337.ma>
2023-11-03 15:48:16 +0100

    strncmp need's to return 0 if n = 0

--- ft_memchr.c
+++ ft_memchr.c
@@ -14,13 +14,17 @@
 
 void	*ft_memchr(const void *s, int c, size_t n)
 {
-	size_t	i;
+	size_t			i;
+	unsigned char	*str;
+	unsigned char	var;
 
 	i = 0;
+	str = (unsigned char *)s;
+	var = (unsigned char)c;
 	while (i < n)
 	{
-		if (((char *)s)[i] == c)
-			return (((char *)s) + i);
+		if (str[i] == var)
+			return (str + i);
 		i++;
 	}
 	return (NULL);
