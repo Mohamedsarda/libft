f55adea8cb57735ed3870fad7590113f5538e801
sardamed <mohamedsarda2001@gmail.com>
2023-11-04 17:09:31 +0100

    substr

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -3,32 +3,37 @@
 /*                                                        :::      ::::::::   */
 /*   ft_strtrim.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: msarda <marvin@42.fr>                      +#+  +:+       +#+        */
+/*   By: medsarda <medsarda@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/02 10:40:37 by msarda            #+#    #+#             */
-/*   Updated: 2023/11/02 10:42:02 by msarda           ###   ########.fr       */
+/*   Updated: 2023/11/04 16:45:49 by medsarda         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char	*ft_strtrim(char const *s1, char const *set)
-{
-	size_t	s1_len;
-	size_t	new_len;
-	size_t	i;
-	char	*dst;
+// char    *ft_strtrim(char const *s1, char const *set)
+// {
+//     int start = 0;
+// 		int j = 0;
+// 		int len_1 = ft_strlen(s1);
+// 		int len_2 = ft_strlen(set);
+// 		int newlen = 0;
+// 		char *dst;
 
-	s1_len = ft_strlen(s1);
-	i = 0;
-	while (i < s1_len && ft_strchr(set, s1[i]))
-		i++;
-	new_len = s1_len - i;
-	while (new_len > 0 && ft_strchr(set, s1[i + new_len - 1]))
-		new_len--;
-	dst = (char *)malloc(new_len + 1);
-	if (!dst)
-		return (NULL);
-	ft_strlcpy(dst, s1 + i, new_len);
-	return (dst);
-}
+// 		while((s1[start] == set[start]) && (start < len_2))
+// 			start++;
+// 		newlen = len_1 - start;
+// 		while((s1[newlen] == set[j]) && (j < len_2))
+// 		{
+// 			newlen--;
+// 			j++;
+// 		}
+// 		printf("%d\n", start);
+// 		printf("%d", newlen);
+//     return (dst);
+// }
+// int main()
+// {
+// 	ft_strtrim("  med  ", "  ");
+// }
--- ft_substr.c
+++ ft_substr.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_substr.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: msarda <marvin@42.fr>                      +#+  +:+       +#+        */
+/*   By: medsarda <medsarda@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/02 10:39:40 by msarda            #+#    #+#             */
-/*   Updated: 2023/11/02 10:40:26 by msarda           ###   ########.fr       */
+/*   Updated: 2023/11/04 17:06:19 by medsarda         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,25 +14,23 @@
 
 char    *ft_substr(char const *s, unsigned int start, size_t len)
 {
-    size_t    i;
-    char    *dst;
+	size_t			i;
+	char			*dst;
 	unsigned int	s_len;
 
-    dst = (char *)malloc(len * sizeof(char) + 1);
-    if (!dst)
-        return (NULL);
-    i = 0;
+	i = 0;
 	s_len = ft_strlen(s);
-    if (len <= s_len)
-    {
-        while (s[start] && (i < len))
-        {
-            dst[i] = s[start];
-            i++;
-            start++;
-        }
-        dst[i] = '\0';
-        return (dst);
-    }
-    return (NULL);
+	if (start >= s_len)
+		start -= s_len;
+	dst = (char *)malloc(len * sizeof(char) + 1);
+	if (!dst)
+		return (NULL);
+	while (s[start] && i < len)
+	{
+		dst[i] = s[start];
+		i++;
+		start++;
+	}
+	dst[i] = '\0';
+	return (dst);
 }
