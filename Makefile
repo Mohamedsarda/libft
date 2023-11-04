821bbf6612e822d8f36eb0a86858593c48ef6d28
Mohamed Sarda <msarda@e1r10p13.1337.ma>
2023-11-04 15:16:12 +0100

    memcmp && bonus

--- Makefile
+++ Makefile
@@ -8,9 +8,12 @@ ft_isalnum.c	ft_memchr.c	ft_strcmp.c	ft_strncmp.c ft_calloc.c\
 ft_isalpha.c	ft_memcpy.c	ft_strdup.c	ft_strnstr.c \
 ft_isascii.c	ft_memmove.c	ft_strlcat.c	ft_strrchr.c	ft_strjoin.c\
 ft_strtrim.c ft_split.c ft_itoa.c ft_putchar_fd.c ft_putstr_fd.c ft_putendl_fd.c\
-ft_putnbr_fd.c ft_strmapi.c ft_striteri.c ft_substr.c\
+ft_putnbr_fd.c ft_strmapi.c ft_striteri.c ft_substr.c ft_memcmp.c\
+
+BSRC = ft_lstnew.c\
 
 OBJS = $(SRCS:.c=.o)
+OBJSB = $(BSRC:.c=.o)
 
 LIBC = ar rcs
 
@@ -23,11 +26,14 @@ all: $(NAME)
 ${NAME}: ${OBJS}
 	${LIBC} ${NAME} ${OBJS}
 
+bonus: ${NAME} ${OBJSB}
+	${LIBC} ${NAME} ${OBJSB}
+
 .c.o:
 	${CC} ${CFLAGS} -c $< -o ${<:.c=.o}
 
 clean:
-	$(RM) $(OBJS)
+	$(RM) $(OBJS) ${OBJSB}
 
 fclean: clean
 	$(RM) $(NAME)
--- libft.h
+++ libft.h
@@ -19,6 +19,11 @@
 # include <stdio.h>
 # include <limits.h>
 
+typedef struct s_list
+{
+	void *content;
+	struct s_list *next;
+}				t_list;
 // Part 1
 int		ft_isalnum(int c);
 int		ft_isalpha(int c);
@@ -35,6 +40,7 @@ void	*ft_memset(void *b, int c, size_t len);
 void	ft_bzero(void *s, size_t n);
 void	*ft_memcpy(void *restrict dst, const void *restrict src, size_t n);
 void	*ft_memmove(void *dst, const void *src, size_t len);
+int		ft_memcmp(const void *s1, const void *s2, size_t n);
 void	*ft_memchr(const void *s, int c, size_t n);
 int		ft_atoi(const char *str);
 size_t	ft_strlcpy(char *restrict dst, const char *restrict src,
@@ -57,5 +63,7 @@ void    ft_putchar_fd(char c, int fd);
 void	ft_putstr_fd(char *s, int fd);
 void	ft_putendl_fd(char *s, int fd);
 void	ft_putnbr_fd(int n, int fd);
+//bonus
+t_list	*ft_lstnew(void *content);
 
 #endif
