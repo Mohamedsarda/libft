42737f094c5d668165e70923f3b950e9871e0949
Mohamed Sarda <msarda@e2r5p5.1337.ma>
2023-11-07 18:43:17 +0100

    strchr && strrchr c need to be casted as a char

--- ft_strchr.c
+++ ft_strchr.c
@@ -16,11 +16,11 @@ char	*ft_strchr(const char *s, int c)
 	int	i;
 
 	i = 0;
-	if (c == '\0')
+	if ((char )c == '\0')
 		return ((char *)s + ft_strlen(s));
 	while (s[i])
 	{
-		if (s[i] == c)
+		if (s[i] == (char )c)
 			return ((char *)s + i);
 		i++;
 	}
